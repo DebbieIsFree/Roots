d9a9b1b3142dbbb8bd52c8174c80923198d419ea
sbpark0611 <sbpark0611@kaist.ac.kr>
2022-07-07 17:22:17 +0900

    1

---
+++ app/src/main/java/com/example/project2/DataLogin.java
@@ -0,0 +1,5 @@
+package com.example.project2;
+
+public class DataLogin {
+    public String email, password, nick_name, create_dt;
+}
---
+++ app/src/main/java/com/example/project2/VoLogin.java
@@ -0,0 +1,6 @@
+package com.example.project2;
+
+public class VoLogin {
+    public String result, message;
+    public DataLogin dataLogin;
+}
