ed8518a11636e0603e7a51af489c5f8f21f13263
wldnj <wldnjs980409@naver.com>
2022-07-09 19:36:11 +0900

    kakao login api

--- app/src/main/java/com/example/project2/MainActivity.java
+++ app/src/main/java/com/example/project2/MainActivity.java
@@ -103,61 +103,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void profileButtonPushed() {
-//        if(UserApiClient.getInstance().isKakaoTalkLoginAvailable(MainActivity.this)){
-//            login();
-//        }
-//        else{
-//            accountLogin();
-//        }
         Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
         startActivity(intent);
     }
-
-//    public void login(){
-//        String TAG = "login()";
-//        UserApiClient.getInstance().loginWithKakaoTalk(MainActivity.this,(oAuthToken, error) -> {
-//            if (error != null) {
-//                Log.e(TAG, "로그인 실패", error);
-//            } else if (oAuthToken != null) {
-//                Log.i(TAG, "로그인 성공(토큰) : " + oAuthToken.getAccessToken());
-//                getUserInfo();
-//            }
-//            return null;
-//        });
-//    }
-//
-//    public void accountLogin() {
-//        String TAG = "accountLogin()";
-//        UserApiClient.getInstance().loginWithKakaoAccount(MainActivity.this, (oAuthToken, error) -> {
-//            if (error != null) {
-//                Log.e(TAG, "로그인 실패", error);
-//            } else if (oAuthToken != null) {
-//                Log.i(TAG, "로그인 성공(토큰) : " + oAuthToken.getAccessToken());
-//                getUserInfo();
-//            }
-//            return null;
-//        });
-//    }
-//    public void getUserInfo(){
-//        String TAG = "getUserInfo()";
-//        UserApiClient.getInstance().me((user, meError) -> {
-//            if (meError != null) {
-//                Log.e(TAG, "사용자 정보 요청 실패", meError);
-//            } else {
-//                System.out.println("로그인 완료");
-//                Log.i(TAG, user.toString());
-//                {
-//                    Log.i(TAG, "사용자 정보 요청 성공" +
-//                            "\n회원번호: "+user.getId() +
-//                            "\n이메일: "+user.getKakaoAccount().getEmail());
-//                }
-//                Account user1 = user.getKakaoAccount();
-//                System.out.println("사용자 계정" + user1);
-//            }
-//            return null;
-//        });
-//    }
-
 
 
     public TabLayout getTabs() {
