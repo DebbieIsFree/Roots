47f5e73afa139e45d5862c64a80429ca41a062dd
wldnj <wldnjs980409@naver.com>
2022-07-09 19:35:02 +0900

    kakao login api

--- app/src/main/java/com/example/project2/LoginActivity.java
+++ app/src/main/java/com/example/project2/LoginActivity.java
@@ -17,19 +17,6 @@ import com.kakao.sdk.user.model.Account;
 import com.kakao.util.exception.KakaoException;
 
 public class LoginActivity extends AppCompatActivity {
-    // 세션 콜백 구현
-//    private ISessionCallback sessionCallback = new ISessionCallback() {
-//        @Override
-//        public void onSessionOpened() {
-//            Log.i("KAKAO_SESSION", "로그인 성공");
-//        }
-//
-//        @Override
-//        public void onSessionOpenFailed(KakaoException exception) {
-//            Log.e("KAKAO_SESSION", "로그인 실패", exception);
-//        }
-//    };
-
     com.kakao.usermgmt.LoginButton kakaoBtn;
 
     @Override
@@ -37,7 +24,6 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
 
-        kakaoBtn = findViewById(R.id.login_button);
         Button btn = findViewById(R.id.btn2);
 
         Log.d("kakaologin" ,"kakao login0");
@@ -54,49 +40,7 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
-//        kakaoBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Log.d("kakaologin" ,"kakao login0");
-//                if(UserApiClient.getInstance().isKakaoTalkLoginAvailable(getApplicationContext())){
-//                    login();
-//                }
-//                else{
-//                    accountLogin();
-//                    Log.d("kakaologin", "kakao login0");
-//                }
-//            }
-//        });
-
-
-//
-//        if(UserApiClient.getInstance().isKakaoTalkLoginAvailable(this)){
-//            login();
-//        }
-//        else{
-//            accountLogin();
-//        }
-
-//         세션 콜백 등록
-//        Session.getCurrentSession().addCallback(sessionCallback);
     }
-//    protected void onDestroy() {
-//        super.onDestroy();
-//
-//        // 세션 콜백 삭제
-//        Session.getCurrentSession().removeCallback(sessionCallback);
-//    }
-//
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-//
-//        // 카카오톡|스토리 간편로그인 실행 결과를 받아서 SDK로 전달
-//        if (Session.getCurrentSession().handleActivityResult(requestCode, resultCode, data)) {
-//            return;
-//        }
-//
-//        super.onActivityResult(requestCode, resultCode, data);
-//    }
 
 
 //    @Override
